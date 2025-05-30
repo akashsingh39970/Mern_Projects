993109fc1dfe49f0531c97004f2d73bdef6b3382
akashsingh39970 <akashsingh39970@gmail.com>
2025-05-30 17:44:25 +0530

    done

--- FILLBASKET/frontend/src/main.jsx
+++ FILLBASKET/frontend/src/main.jsx
@@ -2,10 +2,14 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRoute } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { AppContextProvider } from './context/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRoute>
+  <BrowserRouter>
+  <AppContextProvider>
     <App />
-  </BrowserRoute>,
+  </AppContextProvider>
+    
+  </BrowserRouter>,
 )
