e0ab34ce57c4c88c35bb50f4c1700dad1c69418f
Akash Singh <104356287+akashsingh39970@users.noreply.github.com>
2025-07-15 22:50:47 +0530

    Update userRoutes.js

--- READON/server/routes/userRoutes.js
+++ READON/server/routes/userRoutes.js
@@ -1,50 +1,29 @@
 
 
-// import express from 'express';
+import express from 'express';
 
-// import * as controller from '../controllers/userController.js'; // Importing all functions from userController.js
-// // Ensure the path is correct based on your project structure
-// import authUser from '../middleware/authUsers.js';
-
-
-// const { register, login, isAuth, logout } = controller; // Destructuring the imported functions for easier access
+import * as controller from '../controllers/userController.js'; // Importing all functions from userController.js
+// Ensure the path is correct based on your project structure
+import authUser from '../middleware/authUsers.js';
 
 
-
-// console.log('✅ userRoutes file loaded'); // This should show in your terminal
-
-// const userRoutes = express.Router();
-
-// userRoutes.post('/register', register);
-// userRoutes.post('/login', login);
-// userRoutes.get('/is-auth',authUser, isAuth);
-// userRoutes.get('/logout',authUser, logout);
-
-// userRoutes.get('/test', (req, res) => res.send('🎉 User route is working!'));
+const { register, login, isAuth, logout } = controller; // Destructuring the imported functions for easier access
 
 
 
-// export default userRoutes;
-
-const express = require('express');
-const {
-  register,
-  login,
-  isAuth,
-  logout
-} = require('../controllers/userController.js');
-const authUser = require('../middleware/authUsers.js');
-
-console.log('✅ userRoutes file loaded');
+console.log('✅ userRoutes file loaded'); // This should show in your terminal
 
 const userRoutes = express.Router();
 
 userRoutes.post('/register', register);
 userRoutes.post('/login', login);
-userRoutes.get('/is-auth', authUser, isAuth);
-userRoutes.get('/logout', authUser, logout);
+userRoutes.get('/is-auth',authUser, isAuth);
+userRoutes.get('/logout',authUser, logout);
 
 userRoutes.get('/test', (req, res) => res.send('🎉 User route is working!'));
 
-module.exports = userRoutes;
+
+
+export default userRoutes;
+
 
