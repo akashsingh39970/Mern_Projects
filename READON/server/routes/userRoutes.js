a3213fa11b9b431c3b9aba07ca01a72de6d38948
Akash Singh <akashsingh39970@gmail.com>
2025-07-23 21:40:21 +0530

    done

---
+++ READON/server/routes/userRoutes.js
@@ -0,0 +1,29 @@
+
+
+import express from 'express';
+
+import * as controller from '../controllers/userController.js'; // Importing all functions from userController.js
+// Ensure the path is correct based on your project structure
+import authUser from '../middleware/authUsers.js';
+
+
+const { register, login, isAuth, logout } = controller; // Destructuring the imported functions for easier access
+
+
+
+console.log('✅ userRoutes file loaded'); // This 
+
+const userRoutes = express.Router();
+
+userRoutes.post('/register', register);
+userRoutes.post('/login', login);
+userRoutes.get('/is-auth',authUser, isAuth);
+userRoutes.get('/logout',authUser, logout);
+
+userRoutes.get('/test', (req, res) => res.send('🎉 User route is working!'));
+
+
+
+export default userRoutes;
+
+
