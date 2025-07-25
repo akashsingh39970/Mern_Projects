4ffaf78d6e2b63380843831ffe5027fb600be423
Akash Singh <104356287+akashsingh39970@users.noreply.github.com>
2025-07-25 16:53:47 +0530

    Update AddAddress.jsx

--- READON/frontend/src/page/AddAddress.jsx
+++ READON/frontend/src/page/AddAddress.jsx
@@ -43,7 +43,7 @@ const AddAddress = () => {
         e.preventDefault();
         // handle submission logic here
         try {
-            const { data } = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/address/add`, {
+            const { data } = await axios.post('/api/address/add', {
                 address: address,
                 userId: user._id
             });
