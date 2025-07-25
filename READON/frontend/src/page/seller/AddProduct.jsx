c390fc49a0afdeb78513714528d5a2ab8091d1ab
Akash Singh <104356287+akashsingh39970@users.noreply.github.com>
2025-07-25 16:51:21 +0530

    Update AddProduct.jsx

--- READON/frontend/src/page/seller/AddProduct.jsx
+++ READON/frontend/src/page/seller/AddProduct.jsx
@@ -36,7 +36,7 @@ const AddProduct = () => {
                 formData.append('images', files[i])
              }
 
-             const {data} = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/product/add`, formData)
+             const {data} = await axios.post('/api/product/add', formData)
              
              if(data.success){
                 toast.success(data.message);
