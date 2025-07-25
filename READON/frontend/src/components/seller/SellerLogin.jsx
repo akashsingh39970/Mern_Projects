9f4cae0a4a469b592beecad76f3771b85d9a0c46
Akash Singh <104356287+akashsingh39970@users.noreply.github.com>
2025-07-25 16:45:48 +0530

    Update SellerLogin.jsx

--- READON/frontend/src/components/seller/SellerLogin.jsx
+++ READON/frontend/src/components/seller/SellerLogin.jsx
@@ -10,7 +10,7 @@ const SellerLogin = () => {
       const onSumbitHandler = async (event) =>{
         try {
           event.preventDefault();
-          const {data} = await axios.post( `${import.meta.env.VITE_BACKEND_URL}/api/seller/login`, {email, password })
+          const {data} = await axios.post( '/api/seller/login', {email, password })
         
           if(data.success){
             setIsSeller(true);
